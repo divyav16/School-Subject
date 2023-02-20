f6d479f35295f59b2ba28684de70aaf3a253d37b
divya vandra <divyavandra@gmail.com>
2023-02-20 20:27:10 +0000

    schoolthings

---
+++ Springterm.java
@@ -0,0 +1,15 @@
+package SeasonalProject;
+//Schools will more focus on planting during this term
+
+public class Springterm {
+    public static void main(String[] args) {
+        System.out.println("botany");
+        System.out.println("Different parts of flowering parts");
+        System.out.println("Water Transportation in plants");
+        System.out.println("Pollination in flowering plants");
+        System.out.println("Seed dispersal");
+    }
+    /* These are the main parts to focus in this Spring Term
+    and everyone has to make a project on that.
+     */
+}
---
+++ Summerterm.java
@@ -0,0 +1,15 @@
+package SeasonalProject;
+//All the sports days, Activities,Drama and Parents meeting will take place during this term.
+
+public class Summerterm {
+    public static void main(String[] args) {
+        System.out.println("Sports day on 18th May ");
+        System.out.println("Drama on 11th June");
+        System.out.println("Parents meeting on 10th July");
+
+        /* At the end of the term there will be a Funfair day for kids
+        we will need some parents volunteer for that
+         */
+
+    }
+}
